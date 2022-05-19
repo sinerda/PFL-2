8470678282593fc6e004526eef0e392e872723d9
Andrey Sinyavskiy <sinerda@gmail.com>
2022-05-19 18:22:47 +0300

    section "about". pt.1 (without js)

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// function openTab(evt, IdName) {
+//   // Declare all variables
+//   let i, tabcontent, tablinks;
+
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("about__tab-content-inner");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("about__tab-links");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(IdName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
