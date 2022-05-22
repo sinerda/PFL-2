c28863dc0a82c97678f55ee27a9a1e1dbe3e7572
Andrey Sinyavskiy <sinerda@gmail.com>
2022-05-22 23:57:59 +0300

    "about", pt.2

--- js/main.js
+++ js/main.js
@@ -1,23 +1,31 @@
-// function openTab(evt, IdName) {
-//   // Declare all variables
-//   let i, tabcontent, tablinks;
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("default").click();
 
-//   // Get all elements with class="tabcontent" and hide them
-//   tabcontent = document.getElementsByClassName("about__tab-content-inner");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
 
-//   // Get all elements with class="tablinks" and remove the class "active"
-//   tablinks = document.getElementsByClassName("about__tab-links");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
+function openTab(evt, IdName) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
 
-//   // Show the current tab, and add an "active" class to the button that opened the tab
-//   document.getElementById(IdName).style.display = "block";
-//   evt.currentTarget.className += " active";
-// }
+  // А мне этот блок совсем и не нужен, хе-хе
+  // 
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("about__tab-content-inner");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("about__tab-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("about__tab-links--active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(IdName).style.display = "flex";
+  evt.currentTarget.className += " about__tab-links--active";
+}
+
+
 $(function () {
   $('.products-slider').slick({
     variableWidth: true,
