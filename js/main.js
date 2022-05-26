ec284ced8d64ffab9eed9ef6879e665ce431be21
Andrey Sinyavskiy <sinerda@gmail.com>
2022-05-26 08:55:30 +0300

    "faq"

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("default").click();
 
-
 function openTab(evt, IdName) {
-  // Declare all variables
   let i, tabcontent, tablinks;
 
-  // А мне этот блок совсем и не нужен, хе-хе
-  // 
-  // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("about__tab-content-inner");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("about__tab-links");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace("about__tab-links--active", "");
   }
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(IdName).style.display = "flex";
   evt.currentTarget.className += " about__tab-links--active";
 }
@@ -48,3 +40,18 @@ $(function () {
   $(".slick-next").html("");
   $(".slick-prev").html("");
 })
+
+
+let acc = document.getElementsByClassName("accordion__btn");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("accordion__btn--active");
+
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
